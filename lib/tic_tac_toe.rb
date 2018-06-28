b53d62643e51050b92998fd08f413fd4023614fd
Emily Jennings <emily.jennings@flatironschool.com>
2018-06-28 18:29:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -159,7 +159,7 @@ class TicTacToe
     end
 
     if won?
-      puts "Congratulations #{winner(@board)}!"
+      puts "Congratulations #{winner}!"
     else draw?
       puts "Cat's Game!"
     end
